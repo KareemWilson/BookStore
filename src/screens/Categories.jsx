cc2947e058c6c6da8b87379f0e4ecf57194f24c1
KareemWilson <kareemmagdywilson@gmail.com>
2022-11-04 14:40:44 +0200

    Fix linters Error

--- src/screens/Categories.jsx
+++ src/screens/Categories.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function Categories() {
-  return <div>Categories</div>;
+  return (
+    <>
+      <div>Categories</div>
+      <button type="button">Check status</button>
+    </>
+  );
 }
 
 export default Categories;
