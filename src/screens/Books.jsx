00c69befdc558fa555798627a00bd4e9b5d95b95
KareemWilson <kareemmagdywilson@gmail.com>
2022-11-10 11:57:03 +0200

    Render books from redux store

--- src/screens/Books.jsx
+++ src/screens/Books.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Book from '../components/Book/Book';
 import Form from '../components/Form/Form';
 
 function Books() {
+  const books = useSelector((state) => state.books);
   return (
     <>
       <h1>Books List</h1>
-      <Book title="good title" author="good author" />
-      <Form />
+      {books.map((book) => (
+        <Book key={book.id} title={book.title} author={book.author} id={book.id} />
+      ))}
+      <Form id={books.length + 1} />
     </>
   );
 }
