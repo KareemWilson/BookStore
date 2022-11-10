59113d8579dc24f49c4466d3d68c96d83f773d28
KareemWilson <kareemmagdywilson@gmail.com>
2022-11-10 11:58:12 +0200

    Make remove functionality

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,24 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../../redux/books/books';
 
 function Book(props) {
-  const { title, author } = props;
+  const { title, author, id } = props;
+  const dispatch = useDispatch();
   return (
     <>
       <div>{title}</div>
       <p>{`by ${author}`}</p>
-      <button type="button">Remove</button>
+      <button type="button" onClick={() => dispatch(removeBook(id))}>Remove</button>
     </>
   );
 }
 
 export default Book;
 Book.defaultProps = {
+  id: null,
   title: null,
   author: null,
 };
 
 Book.propTypes = {
+  id: PropTypes.number,
   title: PropTypes.string,
   author: PropTypes.string,
 };
