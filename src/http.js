62379be30c478ea6234c587f456d85a94a455cbe
KareemWilson <kareemmagdywilson@gmail.com>
2022-11-12 01:04:47 +0200

    Delete the header key from axios

--- src/http.js
+++ src/http.js
@@ -2,8 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
   baseURL:
-    'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/T4OzABrRKCy1XBnIRlpr/books',
-  headers: {
-    'Content-type': 'application/json',
-  },
+    'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/yVcEHPmCo4zjo1B6WRPf/books',
 });
